832cc02bdeeffdd70dd79e33e174a1dc369fc110
Thang Tran <trathanl@amazon.com>
2024-05-25 13:32:32 -0700

    Added user pool and info

--- amplify-cognito-next/src/app/hooks/use-auth-user.ts
+++ amplify-cognito-next/src/app/hooks/use-auth-user.ts
@@ -4,6 +4,7 @@ import {
   getCurrentUser,
 } from "aws-amplify/auth";
 import { useEffect, useState } from "react";
+import { DEFAULT_ADMIN_GROUP } from "@/app/amplify-cognito-config";
 
 export default function useAuthUser() {
   const [user, setUser] = useState<Record<string, any>>();
@@ -21,7 +22,7 @@ export default function useAuthUser() {
       };
       const groups = session.tokens.accessToken.payload["cognito:groups"];
       // @ts-ignore
-      user.isAdmin = Boolean(groups && groups.includes("Admins"));
+      user.isAdmin = Boolean(groups && groups.includes(DEFAULT_ADMIN_GROUP));
       setUser(user);
     }
 
